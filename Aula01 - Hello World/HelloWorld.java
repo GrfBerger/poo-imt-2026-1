2d12ea892832a377296e85f97510ad2c2557d49a
Gustavo <grifoberger@gmail.com>
2026-07-04 15:37:22 -0300

    Add files via upload

---
+++ Aula01 - Hello World/HelloWorld.java
@@ -0,0 +1,18 @@
+// "class": Palavra-chave usada para avisar ao Java que estamos criando uma classe.
+// "HelloWorld": Nome da classe criada. O arquivo salvo deve ter exatamente esse nome (HelloWorld.java).
+// "{": A chave abre o bloco de código que pertence a esta classe.
+public class HelloWorld {
+
+    // Método "main": Método que serve como ponto de partida do programa.
+    // "{": A chave abre o bloco de código de tudo o que vai acontecer dentro do "main".
+    public static void main(String[] args) {
+        
+        // "System": Classe nativa do Java que contém ferramentas do sistema.
+        // "println": Método da classe System que imprime o texto passado como parâmetro (entre os parenteses)
+        System.out.println("Hello World");
+        
+    // "}": Fecha o bloco de código do método "main".    
+    }
+
+// "}": Fecha o bloco de código da classe "HelloWorld", finalizando o programa.
+}
